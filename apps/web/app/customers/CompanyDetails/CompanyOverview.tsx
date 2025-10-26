6459ce001ce9f8dffdda971464e3bfe2aff57a0c
mhkasif <mhkasif97@gmail.com>
2025-10-27 00:01:10 +0500

    fix deployment issue

--- apps/web/app/customers/CompanyDetails/CompanyOverview.tsx
+++ apps/web/app/customers/CompanyDetails/CompanyOverview.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CollapsibleSection } from '../../../reusableComponents/CollapsibleSection';
+import { CollapsibleSection } from '../../reusableComponents/CollapsibleSection';
 
 interface Company {
   id: string;
